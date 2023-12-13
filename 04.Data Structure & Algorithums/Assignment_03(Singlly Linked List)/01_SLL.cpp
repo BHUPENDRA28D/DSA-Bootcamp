6c6b30e735b2eced408d2e898d38019fe4bbc7ce
Bhupendra Makode <76209510+BHUPENDRA28D@users.noreply.github.com>
2023-12-13 23:49:00 +0530

    updated content

---
+++ 04.Data Structure & Algorithums/Assignment_03(Singlly Linked List)/01_SLL.cpp
@@ -0,0 +1,43 @@
+#include <iostream>
+using namespace std;
+class Node
+{
+public:
+    int item;
+    Node *next;
+    // Defualt Constructor
+    Node() { item = 0, next = NULL; }
+
+    Node(int item) { this->item = item, next = NULL; }
+    // Node(int item) : item(this->item), next(nullptr) {}
+};
+class SLL
+{
+
+private:
+    Node *start;
+
+public:
+    SLL()
+    {
+        start = NULL;
+    }
+
+    void insertAtHead(Node &, int);
+};
+
+void SLL::insertAtHead(Node &start, int data)
+{
+    Node *temp = new Node(data);
+    temp->item = data;
+    temp->next = start;
+}
+// Main Function.
+int main()
+{
+    Node *node1 = new Node(23);
+    cout << "Created a node with value: " << node1->item << endl;
+    cout << "Created node address values: " << node1->next << endl;
+
+    return 0;
+}
