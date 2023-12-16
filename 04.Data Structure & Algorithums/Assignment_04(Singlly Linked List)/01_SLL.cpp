668d1d582d01d83e148a2ec47876cd0c6c4575ae
Bhupendra Makode <76209510+BHUPENDRA28D@users.noreply.github.com>
2023-12-16 14:14:21 +0530

    insert At start and end methode created

--- 04.Data Structure & Algorithums/Assignment_04(Singlly Linked List)/01_SLL.cpp
+++ 04.Data Structure & Algorithums/Assignment_04(Singlly Linked List)/01_SLL.cpp
@@ -1,4 +1,7 @@
 #include <iostream>
+#include <stdio.h>
+// #include "01_Array_ADT.cpp";
+
 using namespace std;
 class Node
 {
@@ -18,24 +21,40 @@ private:
     Node *start;
 
 public:
-    SLL()
-    {
-        start = NULL;
-    }
-
-    void insertAtHead(Node &, int);
+    SLL() : start(NULL) {}
+    void insertAtStart(int);
+    void insertAtLast(int);
 };
-
-void SLL::insertAtHead(Node &start, int data)
+void SLL::insertAtLast(int data)
 {
-    Node *temp = new Node(data);
-    temp->item = data;
-    // temp->next = start;
+    Node *temp;
+    Node *n = new Node;
+    n->item = data;
+    n->next = NULL;
+
+    if (start == NULL)
+        start = n;
+    else
+        while (temp->next != NULL)
+            temp = temp->next;
+
+    temp->next = n;
 }
+
+void SLL::insertAtStart(int data)
+{
+    Node *n1 = new Node;
+    n1->item = data;
+    n1->next = start;
+    start = n1;
+}
+
 // Main Function.
 int main()
 {
     Node *node1 = new Node(23);
+    Node *node2;
+
     cout << "Created a node with value: " << node1->item << endl;
     cout << "Created node address values: " << node1->next << endl;
 
